82ae76f3c82d4143cdd5b8a8aceede4e448d744c
Juan <juan.gallegomesa@gmail.com>
2020-09-16 20:35:35 -0500

    se crea ejemplo de función

---
+++ appArrow.js
@@ -0,0 +1,13 @@
+let calcularDistancia = (coordenadax, coordenaday) => {
+
+    let cuadradox = Math.pow(coordenadax, 2);
+    let cuadradoy = Math.pow(coordenaday, 2);
+
+    let sumaCuadrados = cuadradox + cuadradoy;
+
+    let raizCuadrada = Math.sqrt(sumaCuadrados);
+
+    return (raizCuadrada);
+}
+
+console.log(calcularDistancia(70, -50));
